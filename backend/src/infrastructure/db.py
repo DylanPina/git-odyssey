ab2e66bbecb9090eb7b2b16e1a11a420c5cc6687
wsulliv8 <35410714+wsulliv8@users.noreply.github.com>
2025-10-18 13:01:38 -0700

    Add db infra and database object parsing

---
+++ backend/src/infrastructure/db.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session
+from typing import Generator
+from infrastructure.settings import settings
+
+
+engine = create_engine(settings.database_url, future=True)
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+
+
+def get_session() -> Generator[Session, None, None]:
+    db: Session = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
