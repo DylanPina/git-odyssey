87ef7a0ce83b3ce26e12e028a4ae15c9888bb5a2
wsulliv8 <35410714+wsulliv8@users.noreply.github.com>
2025-10-18 14:19:33 -0700

    Get port from env

--- backend/src/main.py
+++ backend/src/main.py
@@ -12,11 +12,12 @@ os.environ["PYTHONPATH"] = src_dir
 
 if __name__ == "__main__":
     import uvicorn
+    port = os.getenv("PORT", 8000)
     try:
         uvicorn.run(
             "app:app",
             host="0.0.0.0",
-            port=8000,
+            port=port,
             reload=True,
             log_level="info",
         )
