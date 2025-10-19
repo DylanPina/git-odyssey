7bcb474df44ba52ba42145d96106b78ac7fa24a7
wsulliv8 <35410714+wsulliv8@users.noreply.github.com>
2025-10-18 19:53:47 -0700

    Add frontend to CORS

--- backend/src/infrastructure/settings.py
+++ backend/src/infrastructure/settings.py
@@ -3,14 +3,15 @@ from typing import List
 
 
 class Settings(BaseSettings):
-  database_url: str
-  cors_allow_origins: List[str] = ["*"]
+    database_url: str
+    cors_allow_origins: List[str] = ["*", "https://git-odyssey-1.onrender.com"]
 
-  model_config = SettingsConfigDict(
-    env_file=".env",
-    env_prefix="",
-    case_sensitive=False,
-    extra="ignore",
-  )
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_prefix="",
+        case_sensitive=False,
+        extra="ignore",
+    )
+
 
 settings = Settings()
