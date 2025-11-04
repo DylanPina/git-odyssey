691fd6805fdc0e2715b32891ba0ee774ec5fa6a0
Will Sullivan <wsulliv8@users.noreply.github.com>
2025-11-03 16:21:04 -0800

    Fix connection pooler during create engine

--- backend/src/infrastructure/db.py
+++ backend/src/infrastructure/db.py
@@ -1,6 +1,7 @@
 # from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker TODO: Use async version
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
+from sqlalchemy.pool import NullPool
 
 engine = None
 SessionLocal = None
@@ -8,8 +9,15 @@ SessionLocal = None
 
 def init_db(database_url: str):
     global engine, SessionLocal
-    engine = create_engine(database_url, echo=False)
-    SessionLocal = sessionmaker(bind=engine, expire_on_commit=False)
+    engine = create_engine(
+        database_url,
+        echo=False,
+        poolclass=NullPool,
+        connect_args={"sslmode": "require"},
+    )
+    SessionLocal = sessionmaker(
+        bind=engine, expire_on_commit=False, autoflush=False, autocommit=False
+    )
 
 
 def close_db():
